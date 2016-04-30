8994615dc0015a8f261903f3727e52c4ef30c51d
David Sánchez <davidslufe02@gmail.com>
2016-04-30 20:58:13 +0200

    AMAGIA PARA SANTI

--- VotingSystem_1b/src/main/java/es/uniovi/asw/dbupdate/InsertConfiguration.java
+++ VotingSystem_1b/src/main/java/es/uniovi/asw/dbupdate/InsertConfiguration.java
@@ -4,6 +4,6 @@ import es.uniovi.asw.dbupdate.exception.PersistenceException;
 import es.uniovi.asw.dbupdate.model.ConfigurationElection;
 
 public interface InsertConfiguration {
-
+//magia
 	void saveConfiguration(ConfigurationElection config) throws PersistenceException;
 }
