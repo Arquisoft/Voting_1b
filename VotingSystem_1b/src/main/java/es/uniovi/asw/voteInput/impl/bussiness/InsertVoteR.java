3fc849ea854c26b76440b06c07ab2d3b04a3f3d2
Santiago Martin <santiagomartinagra@gmail.com>
2016-04-30 21:20:05 +0200

    fix

--- VotingSystem_1b/src/main/java/es/uniovi/asw/voteInput/impl/bussiness/InsertVoteR.java
+++ VotingSystem_1b/src/main/java/es/uniovi/asw/voteInput/impl/bussiness/InsertVoteR.java
@@ -5,25 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import es.uniovi.asw.dbupdate.GetVotationOptions;
 import es.uniovi.asw.dbupdate.InsertVote;
-import es.uniovi.asw.dbupdate.getVotationOptions;
-import es.uniovi.asw.dbupdate.impl.InsertVoteP;
 import es.uniovi.asw.dbupdate.model.ConfigurationElection;
-import es.uniovi.asw.dbupdate.model.User;
 import es.uniovi.asw.dbupdate.model.VotableOption;
 import es.uniovi.asw.dbupdate.model.Vote;
-import es.uniovi.asw.dbupdate.repositories.TelematicVoterDAO;
-import es.uniovi.asw.dbupdate.repositories.UserDAO;
-import es.uniovi.asw.dbupdate.repositories.VotableOptionDAO;
-import es.uniovi.asw.dbupdate.repositories.VoteDAO;
 import es.uniovi.asw.voteApplication.impl.exception.InvalidUserException;
-import es.uniovi.asw.voteInput.SelectVote;
 import es.uniovi.asw.voterVote.impl.exception.BusinessException;
 
 @Component
 public class InsertVoteR {
 	@Autowired
-	getVotationOptions vd;
+	GetVotationOptions vd;
 	
 	@Autowired
 	InsertVote aaa;
