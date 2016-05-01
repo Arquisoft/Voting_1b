9ee8ba2fbf3701364939b2b8352c95801c657437
Santiago Martin <santiagomartinagra@gmail.com>
2016-05-01 11:58:54 +0200

    basura sacada

--- VotingSystem_1b/src/main/java/es/uniovi/asw/voteApplication/impl/exception/InvalidUserException.java
+++ VotingSystem_1b/src/main/java/es/uniovi/asw/voteApplication/impl/exception/InvalidUserException.java
@@ -4,9 +4,6 @@ public class InvalidUserException  extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 
-	public InvalidUserException() {
-	}
-
 	public InvalidUserException(String string) {
 		super(string);
 	}
